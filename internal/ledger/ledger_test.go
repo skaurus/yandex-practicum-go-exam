6a847edd6ecb1b7008fa74ad6a9d71d3996f3af8
Dmitry Shalashov <skaurus@gmail.com>
2022-07-03 21:16:00 +0200

    test(ledger): added ledger tests, including accruals and withdrawals

---
+++ internal/ledger/ledger_test.go
@@ -0,0 +1,162 @@
+package ledger
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/rs/zerolog"
+	"github.com/shopspring/decimal"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/skaurus/yandex-practicum-go-exam/internal/db"
+	"github.com/skaurus/yandex-practicum-go-exam/internal/env"
+)
+
+var orderNumberOne = "-1"
+var orderNumberTwo = "-2"
+var userNumberOne uint32 = 1
+var userNumberTwo uint32 = 2
+var negativeDecimal = decimal.New(-42, 0)
+
+func getTestEnv(t *testing.T) Env {
+	err := viper.BindEnv("DATABASE_URI", "DATABASE_URI")
+	assert.Nilf(t, err, "viper.BindEnv has failed: %v", err)
+	viper.SetDefault("DB_QUERY_TIMEOUT", 1*time.Second)
+
+	db, err := db.Connect(context.Background())
+	assert.Nilf(t, err, "connect has failed with error %v", err)
+	assert.NotNil(t, db)
+	assert.NotNil(t, db.Handle())
+	assert.IsType(t, &pgxpool.Pool{}, db.Handle())
+
+	_, err = db.Exec(context.Background(), "DELETE FROM ledger WHERE value < 0")
+	assert.Nilf(t, err, "connect has failed with error %v", err)
+
+	zlog := zerolog.New(os.Stdout)
+	env := env.Init(db, &zlog)
+	return Env{&env}
+}
+
+func Test_AddTransaction(t *testing.T) {
+	runEnv := getTestEnv(t)
+
+	transaction, err := runEnv.AddTransaction(
+		context.Background(),
+		int(userNumberOne), orderNumberOne,
+		TransactionCredit, &negativeDecimal,
+	)
+	assert.Nilf(t, err, "order create failed with error %s", err)
+	assert.IsType(t, &Transaction{}, transaction)
+	assert.Equal(t, userNumberOne, transaction.UserID)
+	assert.Equal(t, orderNumberOne, transaction.OrderNumber)
+	assert.Equal(t, TransactionCredit, transaction.Operation)
+	assert.True(t, negativeDecimal.Equal(*transaction.Value))
+
+	transaction = nil
+	transactions, err := runEnv.GetList(
+		context.Background(),
+		"user_id = $1 AND order_number = $2",
+		"LIMIT 1",
+		userNumberOne, orderNumberOne,
+	)
+	assert.Nilf(t, err, "order get failed with error %s", err)
+	assert.Equal(t, 1, len(*transactions))
+	transaction = &(*transactions)[0]
+	assert.IsType(t, &Transaction{}, transaction)
+	assert.Equal(t, userNumberOne, transaction.UserID)
+	assert.Equal(t, orderNumberOne, transaction.OrderNumber)
+	assert.Equal(t, TransactionCredit, transaction.Operation)
+	assert.True(t, negativeDecimal.Equal(*transaction.Value))
+}
+
+func Test_CreateAndListByUserID(t *testing.T) {
+	runEnv := getTestEnv(t)
+
+	transaction1, err := runEnv.AddTransaction(
+		context.Background(),
+		int(userNumberOne), orderNumberOne,
+		TransactionCredit, &negativeDecimal,
+	)
+	assert.Nilf(t, err, "order create failed with error %s", err)
+	assert.IsType(t, &Transaction{}, transaction1)
+	assert.Equal(t, userNumberOne, transaction1.UserID)
+	assert.Equal(t, orderNumberOne, transaction1.OrderNumber)
+	assert.Equal(t, TransactionCredit, transaction1.Operation)
+	assert.True(t, negativeDecimal.Equal(*transaction1.Value))
+
+	biggerNegativeDecimal := negativeDecimal.Mul(decimal.New(2, 0))
+	transaction2, err := runEnv.AddTransaction(
+		context.Background(),
+		int(userNumberOne), orderNumberTwo,
+		TransactionCredit, &biggerNegativeDecimal,
+	)
+	assert.Nilf(t, err, "order create failed with error %s", err)
+	assert.IsType(t, &Transaction{}, transaction2)
+	assert.Equal(t, userNumberOne, transaction2.UserID)
+	assert.Equal(t, orderNumberTwo, transaction2.OrderNumber)
+	assert.Equal(t, TransactionCredit, transaction2.Operation)
+	assert.True(t, biggerNegativeDecimal.Equal(*transaction2.Value))
+
+	transactions, err := runEnv.GetListByUserID(context.Background(), int(userNumberOne))
+	assert.Nilf(t, err, "orders list failed with error %s", err)
+	assert.IsType(t, &[]Transaction{}, transactions)
+	assert.Equal(t, 2, len(*transactions))
+	assert.Equal(t, orderNumberOne, (*transactions)[0].OrderNumber)
+	assert.Equal(t, userNumberOne, (*transactions)[0].UserID)
+	assert.Equal(t, TransactionCredit, (*transactions)[0].Operation)
+	assert.True(t, negativeDecimal.Equal(*(*transactions)[0].Value))
+	assert.Equal(t, orderNumberTwo, (*transactions)[1].OrderNumber)
+	assert.Equal(t, userNumberOne, (*transactions)[1].UserID)
+	assert.Equal(t, TransactionCredit, (*transactions)[1].Operation)
+	assert.True(t, biggerNegativeDecimal.Equal(*(*transactions)[1].Value))
+}
+
+func Test_CreateAndList(t *testing.T) {
+	runEnv := getTestEnv(t)
+
+	transaction1, err := runEnv.AddTransaction(
+		context.Background(),
+		int(userNumberTwo), orderNumberOne,
+		TransactionDebit, &negativeDecimal,
+	)
+	assert.Nilf(t, err, "order create failed with error %s", err)
+	assert.IsType(t, &Transaction{}, transaction1)
+	assert.Equal(t, userNumberTwo, transaction1.UserID)
+	assert.Equal(t, orderNumberOne, transaction1.OrderNumber)
+	assert.Equal(t, TransactionDebit, transaction1.Operation)
+	assert.True(t, negativeDecimal.Equal(*transaction1.Value))
+
+	biggerNegativeDecimal := negativeDecimal.Mul(decimal.New(2, 0))
+	transaction2, err := runEnv.AddTransaction(
+		context.Background(),
+		int(userNumberTwo), orderNumberTwo,
+		TransactionDebit, &biggerNegativeDecimal,
+	)
+	assert.Nilf(t, err, "order create failed with error %s", err)
+	assert.IsType(t, &Transaction{}, transaction2)
+	assert.Equal(t, userNumberTwo, transaction2.UserID)
+	assert.Equal(t, orderNumberTwo, transaction2.OrderNumber)
+	assert.Equal(t, TransactionDebit, transaction2.Operation)
+	assert.True(t, biggerNegativeDecimal.Equal(*transaction2.Value))
+
+	transactions, err := runEnv.GetList(
+		context.Background(),
+		"value < 0",
+		"ORDER BY processed_at DESC",
+	)
+	assert.Nilf(t, err, "orders list failed with error %s", err)
+	assert.IsType(t, &[]Transaction{}, transactions)
+	assert.Equal(t, 2, len(*transactions))
+	assert.Equal(t, orderNumberTwo, (*transactions)[0].OrderNumber)
+	assert.Equal(t, userNumberTwo, (*transactions)[0].UserID)
+	assert.Equal(t, TransactionDebit, (*transactions)[0].Operation)
+	assert.True(t, biggerNegativeDecimal.Equal(*(*transactions)[0].Value))
+	assert.Equal(t, orderNumberOne, (*transactions)[1].OrderNumber)
+	assert.Equal(t, userNumberTwo, (*transactions)[1].UserID)
+	assert.Equal(t, TransactionDebit, (*transactions)[1].Operation)
+	assert.True(t, negativeDecimal.Equal(*(*transactions)[1].Value))
+}
--- internal/users/users_test.go
+++ internal/users/users_test.go
@@ -14,6 +14,8 @@ import (
 
 	"github.com/skaurus/yandex-practicum-go-exam/internal/db"
 	"github.com/skaurus/yandex-practicum-go-exam/internal/env"
+	"github.com/skaurus/yandex-practicum-go-exam/internal/ledger"
+	"github.com/skaurus/yandex-practicum-go-exam/internal/orders"
 )
 
 var userLogin = ""
@@ -60,4 +62,108 @@ func Test_CreateAndGet(t *testing.T) {
 	assert.Equal(t, HashPassword(userPassword), user.Password)
 	assert.Equal(t, zeroDecimal, user.Balance)
 	assert.Equal(t, zeroDecimal, user.Withdrawn)
+}
+
+func Test_AccrueAndWithdraw(t *testing.T) {
+	usersEnv := getTestEnv(t)
+	globalEnv := env.Init(usersEnv.DB(), usersEnv.Logger())
+	ordersEnv := orders.Env{&globalEnv}
+	ledgerEnv := ledger.Env{&globalEnv}
+
+	orderNumber := "-1"
+	_, err := usersEnv.DB().Exec(context.Background(), "DELETE FROM orders WHERE number::text = $1", orderNumber)
+	assert.Nilf(t, err, "cleaning up db has failed with error %v", err)
+
+	accrual := decimal.New(2*42, 0)
+	withdraw := decimal.New(1*42, 0)
+
+	order := &orders.Order{
+		Number:  orderNumber,
+		UserID:  1,
+		Status:  orders.StatusProcessed,
+		Accrual: &accrual,
+	}
+
+	err = ordersEnv.Accrue(context.Background(), ledgerEnv, order)
+	assert.ErrorContains(t, err, orders.ErrNoSuchOrder.Error())
+
+	err = usersEnv.Withdraw(context.Background(), ledgerEnv, int(order.UserID), orderNumber, &withdraw)
+	assert.ErrorContains(t, err, ErrNoSuchUser.Error())
+
+	order, err = ordersEnv.Create(context.Background(), orderNumber, 1)
+	assert.Nil(t, err)
+	assert.IsType(t, &orders.Order{}, order)
+	assert.Equal(t, orderNumber, order.Number)
+
+	rowsAffected, err := ordersEnv.Update(
+		context.Background(),
+		orders.OrderUpdate{orderNumber, orders.StatusProcessed, &accrual},
+	)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, rowsAffected)
+
+	order, err = ordersEnv.GetByNumber(context.Background(), orderNumber)
+	assert.Nil(t, err)
+	assert.IsType(t, &orders.Order{}, order)
+	assert.Equal(t, orderNumber, order.Number)
+	assert.Equal(t, orders.StatusProcessed, order.Status)
+	assert.True(t, order.Accrual.Equal(accrual))
+
+	err = ordersEnv.Accrue(context.Background(), ledgerEnv, order)
+	assert.ErrorContains(t, err, ErrNoSuchUser.Error())
+
+	err = usersEnv.Withdraw(context.Background(), ledgerEnv, int(order.UserID), orderNumber, &withdraw)
+	assert.ErrorContains(t, err, ErrNoSuchUser.Error())
+
+	user, err := usersEnv.Create(context.Background(), Request{"", ""})
+	assert.Nil(t, err)
+	assert.IsType(t, &User{}, user)
+	assert.True(t, user.ID > 0)
+
+	order.UserID = user.ID
+
+	err = ordersEnv.Accrue(context.Background(), ledgerEnv, order)
+	assert.Nil(t, err)
+
+	transactions, err := ledgerEnv.GetList(
+		context.Background(),
+		"order_number::text = $1 AND user_id = $2",
+		"",
+		order.Number, user.ID,
+	)
+	assert.Nil(t, err)
+	assert.IsType(t, &[]ledger.Transaction{}, transactions)
+	assert.Equal(t, 1, len(*transactions))
+	transaction := (*transactions)[0]
+	assert.Equal(t, ledger.TransactionDebit, transaction.Operation)
+	assert.True(t, transaction.Value.Equal(accrual))
+
+	user, err = usersEnv.GetByID(context.Background(), int(user.ID))
+	assert.Nil(t, err)
+	assert.Equal(t, order.UserID, user.ID)
+	assert.True(t, user.Balance.Equal(accrual))
+	assert.True(t, user.Withdrawn.Equal(decimal.New(0, 0)))
+
+	err = usersEnv.Withdraw(context.Background(), ledgerEnv, int(order.UserID), orderNumber, &withdraw)
+	assert.Nil(t, err)
+
+	transactions, err = ledgerEnv.GetList(
+		context.Background(),
+		"order_number::text = $1 AND user_id = $2",
+		"ORDER BY processed_at ASC",
+		order.Number, user.ID,
+	)
+	assert.Nil(t, err)
+	assert.IsType(t, &[]ledger.Transaction{}, transactions)
+	assert.Equal(t, 2, len(*transactions))
+	assert.Equal(t, ledger.TransactionDebit, (*transactions)[0].Operation)
+	assert.True(t, (*transactions)[0].Value.Equal(accrual))
+	assert.Equal(t, ledger.TransactionCredit, (*transactions)[1].Operation)
+	assert.True(t, (*transactions)[1].Value.Equal(withdraw))
+
+	user, err = usersEnv.GetByID(context.Background(), int(user.ID))
+	assert.Nil(t, err)
+	assert.Equal(t, order.UserID, user.ID)
+	assert.True(t, user.Balance.Equal(accrual.Sub(withdraw)))
+	assert.True(t, user.Withdrawn.Equal(withdraw))
 }
